33e5b3e765708e05e48dc7863a8a0b4810d09013
dona125 <donamndl125@gmail.com>
2026-04-14 19:59:55 +0530

    Add App jsx file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import AuthModal from './components/AuthModal';
+import Home from './pages/Home';
+import Agriculture from './pages/Agriculture';
+import Education from './pages/Education';
+import Medical from './pages/Medical';
+import './index.css';
+
+export default function App() {
+  const [showAuth, setShowAuth] = useState(false);
+
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar onAuthClick={() => setShowAuth(true)} />
+          {showAuth && <AuthModal onClose={() => setShowAuth(false)} />}
+          <Routes>
+            <Route path="/"            element={<Home />}        />
+            <Route path="/agriculture" element={<Agriculture />} />
+            <Route path="/education"   element={<Education />}   />
+            <Route path="/medical"     element={<Medical />}     />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
